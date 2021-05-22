08a676047857b2b302d0d562c87c6e129321c6da
Mauro Falcone <maurofalcone@Mauros-MacBook-Pro.local>
2021-05-22 18:58:02 -0300

    Minimize CSS for production mode

--- webpack.common.js
+++ webpack.common.js
@@ -4,19 +4,6 @@ module.exports = {
   entry: {
     app: './index.tsx',
   },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,8 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
   mode: "production",
@@ -9,6 +11,27 @@ module.exports = merge(common, {
       title: "Production",
       template: "./index.html",
     }),
+    new MiniCssExtractPlugin(),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin({
+        parallel: 4,
+      }),
+    ],
+  },
   devtool: "source-map",
 });
