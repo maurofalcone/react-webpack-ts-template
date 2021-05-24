a741aad353b425d3364bfd229812998d7066bec3
Mauro Falcone <maurofalcone@Mauros-MacBook-Pro.local>
2021-05-24 14:01:21 -0300

    Added babel module resolver plugin for paths

--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,18 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   entry: {
-    app: './index.tsx',
+    app: "./index.tsx",
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
+    alias: {
+      Components: path.resolve("./src/components"),
+    },
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
 };
