b93a8fde449ac553e99b4da2f7e90bb79e63c581
Mauro Falcone <maurofalcone@Mauros-MacBook-Pro.local>
2021-05-24 20:05:18 -0300

    fix component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,6 @@ import { shallow } from "enzyme";
 import App from "./App";
 
 test("App component should exists", () => {
-  // Render a checkbox with label in the document
   const component = shallow(<App />);
 
   expect(component.exists()).toBe(true);
